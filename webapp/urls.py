aa2608231c9adfc7653b3c834b5a0d6ea1ded6d2
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-03-15 19:17:06 +0300

    new app blog

---
+++ webapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from webapp.views import *
+
+app_name = 'webapp'
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('services/', services, name='services'),
+    path('cars/', cars, name='cars'),
+    path('about/', about, name='about'),
+    path('blog/', blog, name='blog'),
+    path('contact/', contact, name='contact'),
+]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'webapp/index.html')
+
+def about(request):
+    return render(request, 'webapp/about.html')
+
+def contact(request):
+    return render(request, 'webapp/contact.html')
+
+def cars(request):
+    return render(request, 'webapp/cars.html')
+
+def services(request):
+    return render(request, 'webapp/services.html')
+
+def single(request):
+    return render(request, 'webapp/../blog/templates/blog/single.html')
+
+def blog(request):
+    return render(request, 'webapp/../blog/templates/blog/blog.html')
